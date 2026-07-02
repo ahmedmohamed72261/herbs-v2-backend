dcef23a471f78a9a861abea0fc0bbf9e313d6ebc
AlbashengineerAhmed <albashengineerahmed@gmail.com>
2026-07-02 22:40:47 +0300

    fix/fix-cors
    fix the allowed origins

--- src/server.js
+++ src/server.js
@@ -28,6 +28,8 @@ const corsOptions = {
             'http://localhost:3001',
             'http://127.0.0.1:3000',
             'http://127.0.0.1:3001',
+            'https://herbs-mosad-front.vercel.app',
+            'https://admin-herbs.vercel.app',
         ];
         
         // Allow requests with no origin (mobile apps, curl requests)
